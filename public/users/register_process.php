c754e9ece3981c0c5f402b1e0c56c3d8308e50e6
MUTO Nao <n@o625.com>
2020-09-04 23:54:40 +0000

    会員登録時にログインID重複登録できないように

--- public/users/register_process.php
+++ public/users/register_process.php
@@ -10,6 +10,16 @@ if (empty($_POST['login_id']) || empty($_POST['password'])) {
 // データベースハンドラ作成
 $dbh = new PDO('mysql:host=mysql;dbname=2020techc_db', '2020techc_username', '2020techc_password');
 
+// 既に同じログインIDで登録されていないか確認 既に登録されていたらエラー
+$select_sth = $dbh->prepare('SELECT login_id, password FROM users WHERE login_id = :login_id LIMIT 1');
+$select_sth->execute([
+    ':login_id' => $_POST['login_id'],
+]);
+if (!empty($select_sth->fetch())) {
+    print('そのログインIDは既に登録されています。<a href="./register.php">戻る</a>');
+    return;
+}
+
 // 会員テーブル users に1行insert
 // SQLインジェクションを防ぐためにプレースホルダを使う
 $insert_sth = $dbh->prepare("INSERT INTO users (login_id, password) VALUES (:login_id, :password)");
